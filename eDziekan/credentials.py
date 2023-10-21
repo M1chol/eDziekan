9b3509ff316487fcdcf65e161f8feaafabb478cf
Michał Kozłowski <micho.kozlowski@gmail.com>
2023-10-21 15:16:20 +0200

    Add project files.

---
+++ eDziekan/credentials.py
@@ -0,0 +1,15 @@
+import os
+
+# IMPORTANT - if you ever change the name of file containing password remember to add it to gitignore
+passFile = "hasla.txt" 
+
+if os.path.exists(passFile):
+    print("Loading credentials")
+    with open(passFile, 'r') as dane:
+        username, password = dane.readline().split()
+    print("Using credentials of ", username)
+else:
+    username = input("Input username: ")
+    password = input("Input password: ")
+    with open(passFile, 'w') as dane:
+        print(username, password, file=dane)
